61cea14e8c4592da2d3cc5840b84ecba69376da4
Moshe Berman <moshberm@gmail.com>
2012-09-07 15:00:04 -0400

    Changed the default movement duration

--- TileParser/Sprite View/MBMovableSpriteView.h
+++ TileParser/Sprite View/MBMovableSpriteView.h
@@ -8,6 +8,30 @@
 
 #import "MBSpriteView.h"
 
+#import "MBSpriteMovementDelegate.h"
+
+#define kMovementDuration 0.45
+
 @interface MBMovableSpriteView : MBSpriteView
+
+@property (nonatomic, assign) id<MBSpriteMovementDelegate> movementDelegate;
+@property (nonatomic, assign) BOOL isMoving;
+
+//
+//  Move N tiles in a given direction.
+//  The size of a tile is determined by the movementDelegate.
+//
+
+- (void) moveInDirection:(MBSpriteMovementDirection)direction distanceInTiles:(NSInteger)distanceInTiles withCompletion:(void (^)())completion;
+
+//
+//  Move one 'tile' in a given direction.
+//  The size of a tile is determined by the movementDelegate.
+//
+
+- (void) moveUpWithCompletion:(void (^)())completion;
+- (void) moveDownWithCompletion:(void (^)())completion;
+- (void) moveLeftWithCompletion:(void (^)())completion;
+- (void) moveRightWithCompletion:(void (^)())completion;
 
 @end
--- TileParser/Sprite View/MBMovableSpriteView.m
+++ TileParser/Sprite View/MBMovableSpriteView.m
@@ -8,24 +8,138 @@
 
 #import "MBMovableSpriteView.h"
 
+#import "MBControllerEvent.h"
+
+#import "MBJoystickView.h"
+
 @implementation MBMovableSpriteView
 
-- (id)initWithFrame:(CGRect)frame
-{
-    self = [super initWithFrame:frame];
+- (id)initWithSpriteName:(NSString *)name{
+    self = [super initWithSpriteName:name];
+    
     if (self) {
-        // Initialization code
+        
+        self.animationDuration = 1.0 * kMovementDuration;
+        _isMoving = NO;
     }
+    
     return self;
 }
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect
-{
-    // Drawing code
+#pragma mark - Movement Methods
+
+//  Move in a direction
+- (void) moveInDirection:(MBSpriteMovementDirection)direction distanceInTiles:(NSInteger)distanceInTiles withCompletion:(void (^)())completion{
+    
+    CGRect oldFrame = [self frame];
+    CGSize tileDimensions = [[self movementDelegate] tileSizeInPoints];
+    
+    CGPoint tileCoordinates = CGPointMake(oldFrame.origin.x/tileDimensions.width, oldFrame.origin.y/tileDimensions.height);
+    
+    //  Calculate the new position
+    if (direction == MBSpriteMovementDirectionHorizontal) {
+        oldFrame.origin.x += (tileDimensions.width * distanceInTiles);
+        tileCoordinates.x += distanceInTiles;
+    }else{
+        oldFrame.origin.y += (tileDimensions.height * distanceInTiles);
+        tileCoordinates.y += distanceInTiles;
+    }
+    
+    if (![[self movementDelegate] tileIsOpenAtCoordinates:tileCoordinates forSprite:self]) {
+        [self resetMovementState];
+        return;
+    }
+    
+    [self startMoving];
+    
+    [UIView animateWithDuration:distanceInTiles*kMovementDuration delay:0 options:UIViewAnimationOptionCurveLinear | UIViewAnimationOptionBeginFromCurrentState animations:^{
+        
+        [self setFrame:oldFrame];
+    }
+                     completion:^(BOOL finished) {
+                         //  Perform whatever the callback warrants
+                         if(completion){
+                             completion();
+                         }
+                         [self resetMovementState];
+                     }];
 }
-*/
+
+- (void) startMoving{
+    [self setIsMoving:YES];
+    [self startAnimating];
+}
+
+- (void) resetMovementState{
+    [self setIsMoving:NO];
+    [self stopAnimation];
+}
+
+#pragma mark - Single Tile Movement
+
+- (void) moveUpWithCompletion:(void (^)()) completion{
+    [self beginAnimationWithKey:@"up"];
+    [self moveInDirection:MBSpriteMovementDirectionVertical distanceInTiles:-1 withCompletion:completion];
+}
+
+- (void) moveDownWithCompletion:(void (^)()) completion{
+    [self beginAnimationWithKey:@"down"];
+    [self moveInDirection:MBSpriteMovementDirectionVertical distanceInTiles:1 withCompletion:completion];
+}
+
+- (void) moveLeftWithCompletion:(void (^)()) completion{
+    [self beginAnimationWithKey:@"left"];
+    [self moveInDirection:MBSpriteMovementDirectionHorizontal distanceInTiles:-1 withCompletion:completion];
+}
+
+- (void) moveRightWithCompletion:(void (^)()) completion{
+    [self beginAnimationWithKey:@"right"];
+    [self moveInDirection:MBSpriteMovementDirectionHorizontal distanceInTiles:1 withCompletion:completion];
+}
+
+#pragma mark - Game Controller Support
+
+- (void)gameController:(MBControllerViewController *)controller buttonsPressedWithSender:(id)sender{
+    
+}
+
+- (void)gameController:(MBControllerViewController *)controller buttonsReleasedWithSender:(id)sender{
+    
+}
+
+- (void)gameController:(MBControllerViewController *)controller joystickValueChangedWithSender:(id)value{
+    
+    if (_isMoving) {
+        return;
+    }
+    
+    MBJoystickView *joystick = value;
+    
+    CGPoint velocity = [joystick velocity];
+    
+    if (velocity.x == 1) {
+        
+        [self setActiveAnimation:@"right"];
+        [self moveRightWithCompletion:nil];
+    }
+    
+    if(velocity.y == 1){
+        
+        [self setActiveAnimation:@"up"];
+        [self moveUpWithCompletion:nil];
+    }
+    
+    if (velocity.x == -1) {
+        
+        [self setActiveAnimation:@"left"];
+        [self moveLeftWithCompletion:nil];
+    }
+    
+    if(velocity.y == -1){
+        
+        [self setActiveAnimation:@"down"];
+        [self moveDownWithCompletion:nil];
+    }
+}
 
 @end

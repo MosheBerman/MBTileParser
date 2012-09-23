4618abe9985e2a3439e4ef874f1496b62ddd0cf7
Moshe Berman <moshberm@gmail.com>
2012-09-23 04:15:45 -0400

    Began working on dialog related classes.

---
+++ TileParser/DialogView/MBDialogView.h
@@ -0,0 +1,16 @@
+//
+//  MBDialogView.h
+//  TileParser
+//
+//  Created by Moshe Berman on 9/23/12.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MBDialogView : UIView
+
+- (id) initWithText:(NSString *)text;
+- (void) showInView:(UIView *)view;
+
+@end

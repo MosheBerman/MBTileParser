b6229604bee1f817ebcd5da27f4813984ebf367e
Moshe Berman <moshberm@gmail.com>
2012-09-23 04:17:28 -0400

    Added new header for the game engine.

---
+++ TileParser/MBGameEngine/MBGameEngine.h
@@ -0,0 +1,24 @@
+//
+//  MBGameEngine.h
+//  TileParser
+//
+//  Created by Moshe Berman on 9/23/12.
+//
+//
+
+#ifndef TileParser_MBGameEngine_h
+#define TileParser_MBGameEngine_h
+
+#import "MBControllerEvent.h"
+
+#import "MBDialogView.h"
+
+#import "MBGameBoyViewController.h"
+
+#import "MBMapViewController.h"
+
+#import "MBMovableSpriteView.h"
+
+#import "MBSelfMovingSpriteView.h"
+
+#endif

9bc733f50beecc2757bfb1db3a9c4ba638c1e00c
Moshe Berman <moshberm@gmail.com>
2012-11-04 02:47:23 -0500

    Updated readme.

---
+++ TileParser/MBGameEngine/MBDialogView/Categories/UILabel+MBDialogString.h
@@ -0,0 +1,13 @@
+//
+//  UILabel+MBDialogString.h
+//  TileParser
+//
+//  Created by Moshe Berman on 10/29/12.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UILabel (MBDialogString)
+
+@end
---
+++ TileParser/MBGameEngine/MBDialogView/Categories/UILabel+MBDialogString.m
@@ -0,0 +1,13 @@
+//
+//  UILabel+MBDialogString.m
+//  TileParser
+//
+//  Created by Moshe Berman on 10/29/12.
+//
+//
+
+#import "UILabel+MBDialogString.h"
+
+@implementation UILabel (MBDialogString)
+
+@end

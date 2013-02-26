59857875dc9f1c27297eb5af0d919a394caf8ba3
Moshe Berman <moshberm@gmail.com>
2013-02-26 18:59:47 -0500

    Add sprite name to the sprite data class.

--- TileParser/MBGameEngine/MBGameState/MBSpriteState.h
+++ TileParser/MBGameEngine/MBGameState/MBSpriteState.h
@@ -8,6 +8,12 @@
 
 #import <Foundation/Foundation.h>
 
+#import "MBSpriteMovementDelegate.h"    // Import for direction enum
+
 @interface MBSpriteState : NSObject
+
+@property (nonatomic, assign) MBSpriteMovementDirection direction;  // What direction is the sprite facing
+@property (nonatomic, assign) CGFloat originInTiles;                // Where is the sprite located?
+@property (nonatomic, strong) NSString *name;                       //What is the sprite name?
 
 @end

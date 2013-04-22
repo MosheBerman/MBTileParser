4915c1e936c08c04b51f119eae8636645ea4817a
Moshe Berman <moshberm@gmail.com>
2013-04-22 02:58:29 -0400

    Fixed README.md

---
+++ TileParser/MBGameEngine/MBGameState/MBStateManager.h
@@ -0,0 +1,13 @@
+//
+//  MBStateManager.h
+//  TileParser
+//
+//  Created by Moshe Berman on 4/21/13.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MBStateManager : NSObject
+
+@end
---
+++ TileParser/MBGameEngine/MBGameState/MBStateManager.m
@@ -0,0 +1,13 @@
+//
+//  MBStateManager.m
+//  TileParser
+//
+//  Created by Moshe Berman on 4/21/13.
+//
+//
+
+#import "MBStateManager.h"
+
+@implementation MBStateManager
+
+@end
